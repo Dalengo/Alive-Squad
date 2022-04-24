ead6f753ce3b18099b4fb43299c099f397f9efda
Yanis1m <yanis.martin@epita.fr>
2022-04-24 22:01:41 +0200

    Update WeakSpot.cs

--- Alive-Squad_Game/Assets/Scripts/WeakSpot.cs
+++ Alive-Squad_Game/Assets/Scripts/WeakSpot.cs
@@ -4,12 +4,9 @@ public class WeakSpot : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(transform.parent.gameObject);
-
         if(collision.CompareTag("Player"))
         {
             Destroy(transform.parent.parent.gameObject);
         }
     }
-    
 }
