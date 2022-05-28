9b0d9ff5669140a559598cbb185e64cbbffbd0db
Ange Mercoyrol--Dol <angemd2883@gmail.com>
2022-05-28 18:17:21 +0200

    Revert "Merge branch 'main' into Ange"
    This reverts commit 3c04ae755c97deb5bf6ace0d0e2eed2732549308, reversing
    changes made to ab09e3084c5ac60c282b4f1e134a8a017506984c.

--- Alive-Squad_Game/Assets/Scripts/PlayerSetup.cs
+++ Alive-Squad_Game/Assets/Scripts/PlayerSetup.cs
@@ -10,20 +10,53 @@ public class PlayerSetup : NetworkBehaviour
     {
         if (!isLocalPlayer)
         {
-            for (int i = 0; i < componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
+            DisableComponents();
         }
         else
         {
             sceneCamera = Camera.main;
             sceneCamera.gameObject.SetActive(false);
+
+            CmdSetUsername(transform.name, UserAccountManager.LoggedInUserName);
+        }
+    }
+
+
+    [Command]
+    void CmdSetUsername(string playerID, string username)
+    {
+        Player player = GameManager.GetPlayer(playerID);
+        if (player != null)
+        {
+            Debug.Log(username + "has joined !");
+            player.username = username;
+        }
+        
+    }
+
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string netId = GetComponent<NetworkIdentity>().netId.ToString();
+        Player player = GetComponent<Player>();
+
+
+        GameManager.RegisterPlayer(netId, player);
+    }
+
+    private void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
         }
     }
 
     private void OnDisable()
     {
         sceneCamera.gameObject.SetActive(true);
+        GameManager.UnregisterPlayer(transform.name);
     }
 }
