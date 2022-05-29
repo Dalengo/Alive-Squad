4c5c5b2ddd9d0f32ce0c244d8d2a552daaccde55
Ange Mercoyrol--Dol <angemd2883@gmail.com>
2022-05-29 19:01:42 +0200

    Changements

---
+++ Alive-Squad_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+}
