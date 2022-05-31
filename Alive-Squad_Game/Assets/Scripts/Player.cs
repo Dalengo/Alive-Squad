45cf61283b1dda623fd6c6fa789e583ea2942b03
Ange Mercoyrol--Dol <angemd2883@gmail.com>
2022-05-31 15:43:06 +0200

    Health

--- Alive-Squad_Game/Assets/Scripts/Player.cs
+++ Alive-Squad_Game/Assets/Scripts/Player.cs
@@ -3,6 +3,50 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
+    private bool _isDead = false;
+
+    public bool isDead
+    {
+        get => _isDead;
+        protected set => _isDead = value;
+    }
+
+    [SerializeField]
+    private float maxHealth = 100f;
+    
+    [SyncVar]
+    private float currentHealth;
+
     [SyncVar]
     public string username = "Player";
+
+    private void Awake()
+    {
+        SetDefaults();
+    }
+
+    private void SetDefaults()
+    {
+        currentHealth = maxHealth;
+    }
+    [ClientRpc]
+    public void RPcTakeDamage(float amount)
+    {
+        if (isDead)
+        {
+            return;
+        }
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+    }
+
+
 }
